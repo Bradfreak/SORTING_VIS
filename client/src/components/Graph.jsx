0e25fab4bdf0fbd1355a9d4ee1767a1e38610688
Bharadwaj Rudravelli <17mcme02@uohyd.ac.in>
2021-07-15 18:25:18 +0530

    Random generation

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
+
+function shuffle(array) {
+    let tmp, current, top = array.length;
+    if(top) while(--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+    }
+    return array;
+}
 
 export default function Graph(props) {
+    
+    let array = [];
+    
+    for (let i = 1; i <= props.size; i++){
+        array[i]=i;
+    }
+    array = shuffle(array);
+
     return (
         <div className="graph">
-            <p>{props.size}</p>
-            <p>{props.sort}</p>
+            {array.map((value, key) => {
+                return(<span key={key}>{value}</span>)
+            })}
         </div>
     )
 }
