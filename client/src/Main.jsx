bb0ad1743fb99398a385dbbf1c4249a6c9f5a85c
Bharadwaj Rudravelli <17mcme02@uohyd.ac.in>
2021-07-15 17:55:21 +0530

    Shifting state

--- client/src/Main.jsx
+++ client/src/Main.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Options from './components/Options';
 import Graph from './components/Graph';
 
 export default function Main() {
+    const [size, changeSize] = useState();
+    const [sort, changeSort] = useState();
+
+    function handleChange(event) {
+        const name = event.target.name;
+        const value = event.target.value;
+        if (name === "sort") { changeSort(value); }
+        else { changeSize(value); }
+    }
+
     return (
         <div className="main">
-            <Options />
-            <Graph />
+            <Options 
+             handleChange={handleChange}
+            />
+            <Graph 
+             size={size}
+             sort={sort}
+            />
         </div>
     )
 }
