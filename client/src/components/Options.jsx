b1f2a3fae5818b8b5bf20d394448da2b89bbc27f
Bharadwaj Rudravelli <17mcme02@uohyd.ac.in>
2021-07-15 17:55:55 +0530

    Using handler to change options

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,17 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 
-export default function Options() {
-    const [size, changeSize] = useState();
-    const [sort, changeSort] = useState();
-
-    function handleChange(event) {
-        const name = event.target.name;
-        const value = event.target.value;
-        if (name === "sort") { changeSort(value); }
-        else { changeSize(value); }
-    }
-
+export default function Options(props) {
     return (
         <div className="options">
             <form>
@@ -21,41 +11,39 @@ export default function Options() {
 
                 <div className="form-section"> <h2>Select sorting algorithm</h2>
                     <label htmlFor="0">
-                        <input type="radio" name="sort" id="0" onChange={handleChange} value="0" />
+                        <input type="radio" name="sort" id="0" onChange={props.handleChange} value="0" />
                         Bubble Sort
                     </label>
                     <label htmlFor="1">
-                        <input type="radio" name="sort" id="1" onChange={handleChange} value="1" />
+                        <input type="radio" name="sort" id="1" onChange={props.handleChange} value="1" />
                         Insertion Sort
                     </label>
                     <label htmlFor="2">
-                        <input type="radio" name="sort" id="2" onChange={handleChange} value="2" />
+                        <input type="radio" name="sort" id="2" onChange={props.handleChange} value="2" />
                         Selection Sort
                     </label>
                     <label htmlFor="3">
-                        <input type="radio" name="sort" id="3" onChange={handleChange} value="3" />
+                        <input type="radio" name="sort" id="3" onChange={props.handleChange} value="3" />
                         Merge Sort
                     </label>
                     <label htmlFor="4">
-                        <input type="radio" name="sort" id="4" onChange={handleChange} value="4" />
+                        <input type="radio" name="sort" id="4" onChange={props.handleChange} value="4" />
                         Quick Sort
                     </label>
                     <label htmlFor="5">
-                        <input type="radio" name="sort" id="5" onChange={handleChange} value="5" />
+                        <input type="radio" name="sort" id="5" onChange={props.handleChange} value="5" />
                         Heap Sort
                     </label>
                 </div>
 
                 <div className="form-section"> <h2>Select Range of values</h2>
-                    <input type="range" name="size" id="" min="5" max="1000" onChange={handleChange} />
+                    <input type="range" name="size" id="" min="5" max="1000" onChange={props.handleChange} />
                 </div>
 
                 <div className="form-section">
                     <input type="button" value="Sort" />
                 </div>
             </form>
-            <p>{size}</p>
-            <p>{sort}</p>
         </div>
     )
 }
