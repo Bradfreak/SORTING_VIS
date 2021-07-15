a6c3c251506949893acbe0f32b0ac0c1ab6ac3ce
RohanBera <berarohan88@gmail.com>
2021-07-15 22:42:04 +0530

    Graph: styles (done)

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function shuffle(array) {
     let tmp, current, top = array.length;
-    if(top) while(--top) {
+    if (top) while (--top) {
         current = Math.floor(Math.random() * (top + 1));
         tmp = array[current];
         array[current] = array[top];
@@ -12,19 +12,46 @@ function shuffle(array) {
 }
 
 export default function Graph(props) {
-    
+
     let array = [];
-    
-    for (let i = 1; i <= props.size; i++){
-        array[i]=i;
+
+    for (let i = 1; i <= props.size; i++) {
+        array[i - 1] = i;
     }
     array = shuffle(array);
+    console.log(props.size);
 
     return (
         <div className="graph">
             {array.map((value, key) => {
-                return(<span key={key}>{value}</span>)
+                return (
+                    <span id={value} key={key}>
+                        <Bar
+                            height={value / props.size}
+                            width={1 / props.size}
+                        />
+
+                        {props.size <= 40 && <div className="value">{value}</div>}
+
+                    </span>
+                )
             })}
+        </div>
+    )
+}
+
+function Bar(props) {
+    return (
+        <div
+            className="bar"
+            style={{
+                flex: props.height,
+                // width: props.width
+            }}
+
+        >
+
+
         </div>
     )
 }
