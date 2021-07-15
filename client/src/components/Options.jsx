bcd91f38d36b8a1ec907fdff6271959f927018b7
Bharadwaj Rudravelli <17mcme02@uohyd.ac.in>
2021-07-15 12:07:11 +0530

    Options RIP On to CSS(Prof Bera CHARGEEE!!!)

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -2,29 +2,29 @@ import React, {useState}from 'react'
 
 
 export default function Options() {
-    const [formData, changeFormData] = useState(
-        {sort:'',
-         size:''
-        });
-    
+    const [size, changeSize] = useState();
+    const [sort, changeSort] = useState();
+
     function handleChange(event) {
         const name = event.target.name;
         const value = event.target.value;
-        changeFormData({...formData, [name]: value});
+        if(name === "sort"){changeSort(value);}
+        else{changeSize(value);}
     }
 
     return (
         <div>
             <form>
                 <input type="button" value="Generate Random" />
-                <input type="radio" name="sort" id="" value="0"/>
-                <input type="radio" name="sort" id="" value="1"/>
-                <input type="radio" name="sort" id="" value="2"/>
-                <input type="radio" name="sort" id="" value="3"/>
-                <input type="radio" name="sort" id="" value="4"/>
+                <input type="radio" name="sort" id="" onChange={handleChange} value="0"/>
+                <input type="radio" name="sort" id="" onChange={handleChange} value="1"/>
+                <input type="radio" name="sort" id="" onChange={handleChange} value="2"/>
+                <input type="radio" name="sort" id="" onChange={handleChange} value="3"/>
+                <input type="radio" name="sort" id="" onChange={handleChange} value="4"/>
                 <input type="range" name="size" id="" min="5" max="1000" onChange={handleChange} />
                 <input type="button" value="Sort" />
-                <p>{formData}</p>
+                <p>{size}</p>
+                <p>{sort}</p>
             </form>
         </div>
     )
