92af4c6c450090a2e120cb63fa8f62565009bf79
RohanBera <berarohan88@gmail.com>
2021-07-16 16:03:08 +0530

    Disabling form on submit

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export default function Options(props) {
+    const [disable, toggleDisable] = useState(false);
+
     return (
         <div className="options">
             <form>
@@ -10,38 +12,47 @@ export default function Options(props) {
                 </div> */}
 
                 <div className="form-section"> <h2>Select sorting algorithm</h2>
-                    <label htmlFor="0">
-                        <input type="radio" name="sort" id="0" onChange={props.handleChange} value="0" />
+                    <label className={disable && "disabled"} htmlFor="0">
+                        <input type="radio" name="sort" id="0" onChange={props.handleChange} value="0" disabled={disable} />
                         Bubble Sort
                     </label>
-                    <label htmlFor="1">
-                        <input type="radio" name="sort" id="1" onChange={props.handleChange} value="1" />
+                    <label className={disable && "disabled"} htmlFor="1">
+                        <input type="radio" name="sort" id="1" onChange={props.handleChange} value="1" disabled={disable} />
                         Insertion Sort
                     </label>
-                    <label htmlFor="2">
-                        <input type="radio" name="sort" id="2" onChange={props.handleChange} value="2" />
+                    <label className={disable && "disabled"} htmlFor="2">
+                        <input type="radio" name="sort" id="2" onChange={props.handleChange} value="2" disabled={disable} />
                         Selection Sort
                     </label>
-                    <label htmlFor="3">
-                        <input type="radio" name="sort" id="3" onChange={props.handleChange} value="3" />
+                    <label className={disable && "disabled"} htmlFor="3">
+                        <input type="radio" name="sort" id="3" onChange={props.handleChange} value="3" disabled={disable} />
                         Merge Sort
                     </label>
-                    <label htmlFor="4">
-                        <input type="radio" name="sort" id="4" onChange={props.handleChange} value="4" />
+                    <label className={disable && "disabled"} htmlFor="4">
+                        <input type="radio" name="sort" id="4" onChange={props.handleChange} value="4" disabled={disable} />
                         Quick Sort
                     </label>
-                    <label htmlFor="5">
-                        <input type="radio" name="sort" id="5" onChange={props.handleChange} value="5" />
+                    <label className={disable && "disabled"} htmlFor="5">
+                        <input type="radio" name="sort" id="5" onChange={props.handleChange} value="5" disabled={disable} />
                         Heap Sort
                     </label>
                 </div>
 
                 <div className="form-section"> <h2>Select Range of values</h2>
-                    <input type="range" name="size" id="" min="5" max="150" onChange={props.handleChange} />
+                    <input type="range" name="size" id="" min="5" max="150" onChange={props.handleChange} disabled={disable} />
                 </div>
 
                 <div className="form-section">
-                    <input type="button" value="Sort" onClick={props.submit}/>
+                    <input
+                        type="button"
+                        value="Sort"
+                        onClick={async () => {
+                            toggleDisable(true);
+                            await props.submit();
+                            toggleDisable(false);
+                        }}
+                        disabled={disable}
+                    />
                 </div>
             </form>
         </div>
